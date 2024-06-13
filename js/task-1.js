489cc41e2291e4374b31c8a2ac2598b22889d04d
OlhaDubovenko88 <osmolovskayaolga1@gmail.com>
2024-06-13 22:31:40 +0200

    add initial files

--- js/task-1.js
+++ js/task-1.js
@@ -4,7 +4,7 @@ function isEnoughCapacity(products, containerSize) {
   for (let key of values) {
     totalAmount += key;
   }
-  return totalAmount < containerSize;
+  return totalAmount <= containerSize;
 }
 console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
 
